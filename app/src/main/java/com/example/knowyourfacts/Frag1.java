66d5feab861c4a4920bc9120d7009a277779760c
18016236 <18016236@myrp.edu.sg>
2020-07-23 19:44:20 +0800

    Initial commit

---
+++ app/src/main/java/com/example/knowyourfacts/Frag1.java
@@ -0,0 +1,93 @@
+package com.example.knowyourfacts;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import java.security.AccessController;
+import java.util.Random;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link Frag1#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class Frag1 extends Fragment {
+    TextView Frag1;
+    Button btnChangeColor;
+    ImageView iv;
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public Frag1() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment Frag1.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static Frag1 newInstance(String param1, String param2) {
+        Frag1 fragment = new Frag1();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        final View view = inflater.inflate(R.layout.fragment_frag1, container,false);
+        Frag1 = view.findViewById(R.id.interestingFacts);
+        iv = view.findViewById(R.id.iv);
+        String imageUrl = "https://www.wtffunfact.com/wtf-fun-fact-dead-snail-in-a-beer/";
+        Context context = container.getContext();
+        Picasso.with(context).load(imageUrl).into(iv);
+        btnChangeColor = view.findViewById(R.id.buttonChangeColour);
+        btnChangeColor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Random rnd = new Random();
+                int color = Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
+                view.setBackgroundColor(color);
+            }
+        });
+        return view;
+    }
+
+
+}
---
+++ app/src/main/java/com/example/knowyourfacts/ScheduleNotificationReceiver.java
@@ -0,0 +1,41 @@
+package com.example.knowyourfacts;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+
+import androidx.core.app.NotificationCompat;
+
+public class ScheduleNotificationReceiver extends BroadcastReceiver {
+
+    int reqCode = 12345;
+
+    @Override
+    public void onReceive(final Context context, Intent intent) {
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            NotificationChannel channel = new NotificationChannel("default","Default Channel",NotificationManager.IMPORTANCE_DEFAULT);
+
+            channel.setDescription("This is the notification for Read Later");
+            notificationManager.createNotificationChannel(channel);
+        }
+        Intent i = new Intent(context,MainActivity.class);
+            PendingIntent pIntent = PendingIntent.getActivity(context,reqCode,i,PendingIntent.FLAG_CANCEL_CURRENT);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context,"default");
+        builder.setContentTitle("Time to Read Now!");
+        builder.setContentText("This is a reminder to read again");
+        builder.setSmallIcon(android.R.drawable.ic_dialog_info);
+        builder.setContentIntent(pIntent);
+        builder.setAutoCancel(true);
+
+        Notification n = builder.build();
+        notificationManager.notify(123,n);
+    }
+}
